1b3cf2f86ef82a75816b922760ddbd05a280003e
gyewan90 <roy1022@hanmail.net>
2019-01-24 20:44:22 +0900

    init base code

---
+++ src/main/java/com/gyewan/mircroservicereservation/MircroserviceReservationApplication.java
@@ -0,0 +1,14 @@
+package com.gyewan.mircroservicereservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MircroserviceReservationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MircroserviceReservationApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/com/gyewan/mircroservicereservation/MircroserviceReservationApplicationTests.java
@@ -0,0 +1,17 @@
+package com.gyewan.mircroservicereservation;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MircroserviceReservationApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
